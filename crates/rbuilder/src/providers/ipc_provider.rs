a416e57929c9d54ff8442670871bfc9e1c9ff176
mralj <nikola.mratinic@gmail.com>
2024-12-19 10:44:55 +0100

    Added mock StateProvider

--- crates/rbuilder/src/providers/ipc_provider.rs
+++ crates/rbuilder/src/providers/ipc_provider.rs
@@ -2,17 +2,26 @@ use std::ops::RangeBounds;
 
 use alloy_consensus::Header;
 use alloy_eips::BlockNumberOrTag;
-use alloy_primitives::{BlockHash, BlockNumber, U256};
+use alloy_primitives::{
+    map::{HashMap, HashSet},
+    BlockHash, BlockNumber, Bytes, StorageKey, StorageValue, U256,
+};
 use reth_chainspec::ChainInfo;
 use reth_errors::ProviderResult;
-use reth_primitives::SealedHeader;
+use reth_primitives::{Account, Bytecode, SealedHeader};
 use reth_provider::{
-    BlockHashReader, BlockIdReader, BlockNumReader, HeaderProvider, StateProviderBox,
-    StateProviderFactory,
+    AccountReader, BlockHashReader, BlockIdReader, BlockNumReader, HeaderProvider,
+    StateProofProvider, StateProvider, StateProviderBox, StateProviderFactory, StateRootProvider,
+    StorageRootProvider,
 };
-use revm_primitives::B256;
+use reth_trie::{
+    updates::TrieUpdates, AccountProof, HashedPostState, HashedStorage, MultiProof, StorageProof,
+    TrieInput,
+};
+use revm_primitives::{Address, B256};
 
 pub struct IpcProviderFactory;
+pub struct IPCStateProvider;
 
 impl StateProviderFactory for IpcProviderFactory {
     /// Storage provider for latest block.
@@ -131,6 +140,117 @@ impl HeaderProvider for IpcProviderFactory {
         _range: impl RangeBounds<BlockNumber>,
         _predicate: impl FnMut(&SealedHeader) -> bool,
     ) -> ProviderResult<Vec<SealedHeader>> {
+        unimplemented!()
+    }
+}
+
+impl StateProvider for IPCStateProvider {
+    /// Get storage of given account
+    fn storage(
+        &self,
+        _account: Address,
+        _storage_key: StorageKey,
+    ) -> ProviderResult<Option<StorageValue>> {
+        todo!()
+    }
+
+    ///Get account code by its hash
+    fn bytecode_by_hash(&self, _code_hash: B256) -> ProviderResult<Option<Bytecode>> {
+        todo!()
+    }
+}
+
+// Required by the StateProvider
+impl BlockHashReader for IPCStateProvider {
+    /// Get the hash of the block with the given number. Returns `None` if no block with this number exists
+    fn block_hash(&self, _number: BlockNumber) -> ProviderResult<Option<B256>> {
+        todo!()
+    }
+
+    fn canonical_hashes_range(
+        &self,
+        _start: BlockNumber,
+        _end: BlockNumber,
+    ) -> ProviderResult<Vec<B256>> {
+        unimplemented!()
+    }
+}
+
+// Required by the StateProvider
+impl AccountReader for IPCStateProvider {
+    /// Get basic account information.
+    /// Returns `None` if the account doesn't exist.
+    fn basic_account(&self, _address: Address) -> ProviderResult<Option<Account>> {
+        todo!()
+    }
+}
+
+impl StateRootProvider for IPCStateProvider {
+    fn state_root(&self, _hashed_state: HashedPostState) -> ProviderResult<B256> {
+        unimplemented!()
+    }
+
+    fn state_root_from_nodes(&self, _input: TrieInput) -> ProviderResult<B256> {
+        unimplemented!()
+    }
+
+    fn state_root_with_updates(
+        &self,
+        _hashed_state: HashedPostState,
+    ) -> ProviderResult<(B256, TrieUpdates)> {
+        unimplemented!()
+    }
+
+    fn state_root_from_nodes_with_updates(
+        &self,
+        _input: TrieInput,
+    ) -> ProviderResult<(B256, TrieUpdates)> {
+        unimplemented!()
+    }
+}
+
+impl StorageRootProvider for IPCStateProvider {
+    fn storage_root(
+        &self,
+        _address: Address,
+        _hashed_storage: HashedStorage,
+    ) -> ProviderResult<B256> {
+        unimplemented!()
+    }
+
+    fn storage_proof(
+        &self,
+        _address: Address,
+        _slot: B256,
+        _hashed_storage: HashedStorage,
+    ) -> ProviderResult<StorageProof> {
+        unimplemented!()
+    }
+}
+
+impl StateProofProvider for IPCStateProvider {
+    fn proof(
+        &self,
+        _input: TrieInput,
+        _address: Address,
+        _slots: &[B256],
+    ) -> ProviderResult<AccountProof> {
+        unimplemented!()
+    }
+
+    fn multiproof(
+        &self,
+        _input: TrieInput,
+        _targets: HashMap<B256, HashSet<B256>>,
+    ) -> ProviderResult<MultiProof> {
+        unimplemented!()
+    }
+
+    fn witness(
+        &self,
+        _input: TrieInput,
+        _target: HashedPostState,
+    ) -> ProviderResult<HashMap<B256, Bytes>> {
         unimplemented!()
     }
 }
