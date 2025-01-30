5b0a52245cca87588d0aad925a61152a36d02071
mralj <nikola.mratinic@gmail.com>
2025-01-30 14:38:44 +0100

    maaaaaybe bugfix

--- crates/rbuilder/src/utils/noncer.rs
+++ crates/rbuilder/src/utils/noncer.rs
@@ -1,10 +1,12 @@
 use crate::provider::StateProviderFactory;
 use ahash::HashMap;
 use alloy_primitives::{Address, B256};
+use dashmap::DashMap;
 use parking_lot::Mutex;
 use reth::providers::StateProviderBox;
 use reth_errors::ProviderResult;
 use std::sync::Arc;
+use tracing::{debug, debug_span};
 
 /// Struct to get nonces for Addresses, caching the results.
 /// NonceCache contains the data (but doesn't allow you to query it) and NonceCacheRef is a reference that allows you to query it.
@@ -17,7 +19,7 @@ pub struct NonceCache<P> {
     provider: P,
     // We have to use Arc<Mutex here because Rc are not Send (so can't be used in futures)
     // and borrows don't work when nonce cache is a field in a struct.
-    cache: Arc<Mutex<HashMap<Address, u64>>>,
+    cache: Arc<DashMap<Address, u64>>,
     block: B256,
 }
 
@@ -28,7 +30,7 @@ where
     pub fn new(provider: P, block: B256) -> Self {
         Self {
             provider,
-            cache: Arc::new(Mutex::new(HashMap::default())),
+            cache: Arc::new(DashMap::new()),
             block,
         }
     }
@@ -44,17 +46,24 @@ where
 
 pub struct NonceCacheRef {
     state: StateProviderBox,
-    cache: Arc<Mutex<HashMap<Address, u64>>>,
+    cache: Arc<DashMap<Address, u64>>,
 }
 
 impl NonceCacheRef {
     pub fn nonce(&self, address: Address) -> ProviderResult<u64> {
-        let mut cache = self.cache.lock();
-        if let Some(nonce) = cache.get(&address) {
+        let id: u64 = rand::random();
+        let span = debug_span!("noncer", id, address = address.to_string());
+        let _guard = span.enter();
+        debug!("noncer: get");
+
+        if let Some(nonce) = self.cache.get(&address) {
+            debug!("noncer: cache hit");
             return Ok(*nonce);
         }
+
         let nonce = self.state.account_nonce(&address)?.unwrap_or_default();
-        cache.insert(address, nonce);
+        self.cache.insert(address, nonce);
+        debug!("noncer: got it");
         Ok(nonce)
     }
 }
