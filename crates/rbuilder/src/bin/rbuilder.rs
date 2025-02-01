748c14a5bb6e0c2b50dc2926c161821c34720edd
mralj <nikola.mratinic@gmail.com>
2025-02-01 07:45:09 +0100

    even more worker and builder threads

--- crates/rbuilder/src/bin/rbuilder.rs
+++ crates/rbuilder/src/bin/rbuilder.rs
@@ -9,7 +9,8 @@ use tokio::runtime::Builder;
 
 fn main() -> eyre::Result<()> {
     let runtime = Builder::new_multi_thread()
-        .max_blocking_threads(2048)
+        .worker_threads(32)
+        .max_blocking_threads(4048)
         .enable_all()
         .build()
         .expect("Failed to create runtime");
