c0f0492e269db1f3f320ad3ba82b0e32864bcee4
mralj <nikola.mratinic@gmail.com>
2025-02-23 10:05:07 +0100

    timeout 10ms

--- crates/rbuilder/src/provider/remote_state_provider.rs
+++ crates/rbuilder/src/provider/remote_state_provider.rs
@@ -43,7 +43,7 @@ pub struct RemoteStateProviderFactory {
 
 impl RemoteStateProviderFactory {
     pub fn new(path: &Path) -> Self {
-        let remote_provider = RpcProvider::try_connect(path, Duration::from_millis(300).into())
+        let remote_provider = RpcProvider::try_connect(path, Duration::from_millis(10).into())
             .expect("Can't connect to IPC");
 
         //let future_runner = FutureRunner::new();
